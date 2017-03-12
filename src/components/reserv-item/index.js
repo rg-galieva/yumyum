93e8c8a6ae4213d4ffe12b83ada5641ea722af7c
Zesc <jeddak.cesc@gmail.com>
2017-03-11 16:33:02 -0800

    feature: ReservItem

---
+++ src/components/reserv-item/index.js
@@ -0,0 +1,27 @@
+/**
+ * Created by zesc on 3/11/17.
+ */
+import React, {PropTypes} from 'react'
+
+const ReservItem = (props) => {
+  let {rest_pic, rest_name, user_numb, link} = props;
+
+  return (
+      <div>
+        <img src={rest_pic} alt={rest_name}/>
+        <h2>{rest_name}</h2>
+        <p>Table for: {user_numb}</p>
+        <p>Topic</p>
+        <a href={link}>Description</a>
+        <button>JOIN</button>
+      </div>
+  )
+}
+
+ReservItem.PropTypes = {
+  rest_pic: PropTypes.string,
+  rest_name: PropTypes.string.isRequired,
+  user_numb: PropTypes.number.isRequired
+};
+
+export default ReservItem
---
+++ src/containers/reserv-list-container/index.js
@@ -0,0 +1,21 @@
+/**
+ * Created by zesc on 3/11/17.
+ */
+import React from 'react'
+import ReservItem from '../../components/reserv-item'
+import restaurant_list from '../../test/db/restauran-list.json'
+
+class ReservListContainer extends Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+  render() {
+
+    <ReservItem/>
+  }
+}
+
+
+export default ItemContainer
