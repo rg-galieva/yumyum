7e07b3443aa68ff788456c38df3b8fbb2dfe5936
Zesc <jeddak.cesc@gmail.com>
2017-03-12 12:29:52 -0700

    styling new-res page

--- src/components/res-form/index.js
+++ src/components/res-form/index.js
@@ -9,13 +9,13 @@ class ResForm extends Component {
     constructor() {
         super();
 
-        this.state({
+        this.state = {
             user_id: 1,
             confirmation_number: 1234,
             date_time: "2017-03-12T11:23:01.969Z",
             party_size: 6,
             restaurant_id: 4
-        })
+        }
     }
 
     onSubmit = (ev) => {
@@ -68,18 +68,30 @@ class ResForm extends Component {
                         <input className="mdl-textfield__input" type="number" id="persons"/>
                         <label className="mdl-textfield__label" htmlFor="persons">Number of persons</label>
                     </div>
-                    <label htmlFor="time_start">
-                        <input type="text" name="time_start"/>
-                    </label>
-                    <label htmlFor="time_end">
-                        <input type="text" name="time_end"/>
-                    </label>
-                    <label htmlFor="tags">
-                        <input type="text" name="tags"/>
-                    </label>
-                    <input type="submit" value="Submit"/>
+                    <div className="time-container">
+                        <label htmlFor="time_start"><span>Time: </span>
+                            from
+                            <input type="time" name="time_start" required/>
+                        </label>
+                        <label htmlFor="time_end">
+                            to
+                            <input type="time" name="time_end" required/>
+                        </label>
+                    </div>
+                    <div className="tags">
+                        <label htmlFor="tags">Topics:
+                            <input type="text" name="tags"/>
+                        </label>
+                    </div>
+                    <div className="prefs">
+                        <label htmlFor="prefs">
+                            Preferences:<input type="text" name="pref"/>
+                        </label>
+                    </div>
+                    <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
+                        Button
+                    </button>
                 </form>
-                <Button color="primary">button</Button>
             </div>
         )
     }
